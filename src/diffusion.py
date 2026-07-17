fix loss calculation, fix timestep bug, add wandb loss tracking, WIP: improve sampling

src/diffusion.py
@@ -1,13 +1,10 @@
 import torch
 
 class LinearNoiseScheduler:
-    def __init__(self, num_timesteps: int, beta_start: float, beta_end: float, batch_size: int, img_size: int, img_channels: int, device: str):
+    def __init__(self, num_timesteps: int, beta_start: float, beta_end: float, device: str):
         self.num_timesteps = num_timesteps
         self.beta_start = beta_start
         self.beta_end = beta_end
-        self.batch_size = batch_size
-        self.img_size = img_size
-        self.img_channels = img_channels
         self.device = device
 
         # All these tensors have dimension --> [1000] if num_timesteps = 1000
@@ -20,28 +17,38 @@ class LinearNoiseScheduler:
     def add_noise(self, x_0: torch.Tensor, noise: torch.Tensor, t: torch.Tensor):
         # sqrt_alpha_bars[t] has shape [batch_size] and we need to multiply it with [batch_size, 3, 1024, 1024] image.
         # for broadcasting to work correctly, we are adding dimensions to make it [batch_size, 1, 1, 1]
-        x_0 = x_0.to(self.device)
-        sqrt_alpha_bars = self.sqrt_alpha_bars[t].reshape([-1, 1, 1, 1]).to(self.device)   # Bx1x1x1
-        sqrt_one_minus_alpha_bars = self.sqrt_one_minus_alpha_bars[t].reshape([-1, 1, 1, 1]).to(self.device)   # Bx1x1x1
+
+        batch_size = x_0.shape[0]
+
+        sqrt_alpha_bars = self.sqrt_alpha_bars[t].reshape([batch_size, 1, 1, 1]).to(self.device)   # Bx1x1x1
+        sqrt_one_minus_alpha_bars = self.sqrt_one_minus_alpha_bars[t].reshape([batch_size, 1, 1, 1]).to(self.device)   # Bx1x1x1
+        x_t = sqrt_alpha_bars*x_0 + sqrt_one_minus_alpha_bars*noise
 
         # the direct formula to get x_t from x_0
-        return sqrt_alpha_bars*x_0 + sqrt_one_minus_alpha_bars*noise
+        return x_t
     
-    def sampling(self, unet: torch.nn.Module, batch_size):
-        x_t = torch.randn((batch_size, self.img_channels, self.img_size, self.img_size)).to(self.device)
+    def sampling(self, unet: torch.nn.Module, x_t: torch.Tensor):
+        
+        batch_size, img_channels, img_size = x_t.shape[0], x_t.shape[1], x_t.shape[2]
+
         for t in range(self.num_timesteps-1, -1, -1):
-            if t%50 == 0:
-                print(f"Working on timestep: {t+1}")
+
+            if (t+1)%100 == 0:
+                print(f"Working on timestep: {t+1}/{self.num_timesteps}")
+
             # t is an integer. Want make tensor that contains t with shape -> [batch_size]
             T = torch.full((batch_size, ), t, device=self.device, dtype=torch.long)
             # T = torch.tensor([t]).expand(batch_size).to(self.device)
+
             sigma_1 = torch.sqrt(self.betas[T]) # fixed large variance (choice 1 in paper)
             # sigma_2 = (self.sqrt_one_minus_alpha_bars[T-1]/self.sqrt_one_minus_alpha_bars[T]) * torch.sqrt(self.betas[T])   # tighter posterior variance (choice 2)
-            noise = sigma_1.reshape(-1, 1, 1, 1) * torch.randn((batch_size, self.img_channels, self.img_size, self.img_size)).to(self.device)
+
+            noise = sigma_1.reshape(-1, 1, 1, 1) * torch.randn((batch_size, img_channels, img_size, img_size)).to(self.device)
             unet_pred_noise = unet(x_t, T)
-            print(f"T={t} | Pred Noise Min/Max: {unet_pred_noise.min().item():.3f}, {unet_pred_noise.max().item():.3f}")
-            print(f"T={t} | X_t Min/Max: {x_t.min().item():.3f}, {x_t.max().item():.3f}")
+
             if t == 0:
                 noise = torch.zeros_like(noise).to(self.device)
+
             x_t = (1/torch.sqrt(self.alphas[T])).reshape(-1, 1, 1, 1) * (x_t - ((self.betas[T]/self.sqrt_one_minus_alpha_bars[T]).reshape(-1, 1, 1, 1) * unet_pred_noise)) + noise
+
         return x_t

src/utils.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
-from diffusion import LinearNoiseScheduler
+from src.diffusion import LinearNoiseScheduler
 
 def visualize_forward_diffusion(data_loader: torch.utils.data.DataLoader):
     """Selects a random image from the dataloader and then applies the forward diffusion process to it,

train.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torchvision.datasets import FashionMNIST
 from torchvision.transforms import v2
 from src.unet import UNet
 from src.load_data import TransistorDataset
@@ -9,9 +8,11 @@ from src.diffusion import LinearNoiseScheduler
 from src.utils import save_model_checkpoint
 
 import os
-import numpy as np
-from pathlib import Path
+import wandb
+from dotenv import load_dotenv
 import matplotlib.pyplot as plt
+
+load_dotenv()
 
 # Loading our dataset
 transform = v2.Compose([
@@ -37,8 +38,28 @@ N_HEADS = 8
 NUM_GROUPS = 8
 DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 
+config = {
+    "BATCH_SIZE": BATCH_SIZE,
+    "LR": LR,
+    "EPOCHS": EPOCHS,
+    "NUM_TIMESTEPS": NUM_TIMESTEPS,
+    "BETA_START": BETA_START,
+    "BETA_END": BETA_END,
+    "IMG_SIZE": IMG_SIZE,
+    "IN_CHANNELS": IN_CHANNELS,
+    "BASE_CHANNELS": BASE_CHANNELS,
+    "TIME_EMB_DIM": TIME_EMB_DIM,
+    "EMB_DIM": EMB_DIM,
+    "N_HEADS": N_HEADS,
+    "NUM_GROUPS": NUM_GROUPS,
+    "DEVICE": DEVICE,
+}
+
+wandb.login(key=os.getenv("WANDB_API_KEY"))
+wandb.init(project='DDPM-MVTecAD', config=config)
+
 # Objects
-lns = LinearNoiseScheduler(NUM_TIMESTEPS, BETA_START, BETA_END, BATCH_SIZE, IMG_SIZE, IN_CHANNELS, DEVICE)
+lns = LinearNoiseScheduler(NUM_TIMESTEPS, BETA_START, BETA_END, DEVICE)
 model = UNet(IN_CHANNELS, BASE_CHANNELS, TIME_EMB_DIM, EMB_DIM, N_HEADS, NUM_GROUPS).to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr = LR)
 ema_model = torch.optim.swa_utils.AveragedModel(
@@ -51,6 +72,7 @@ criterion = torch.nn.MSELoss()
 train_loader = DataLoader(dataset, BATCH_SIZE, shuffle=True, num_workers=8)
 
 # Training starts
+fixed_noise = torch.randn((8, IN_CHANNELS, IMG_SIZE, IMG_SIZE), device=DEVICE, requires_grad=False) # Fixed noise to test generated samples
 best_loss = 1000
 losses = []
 
@@ -58,13 +80,14 @@ for epoch in range(EPOCHS):
 
     model.train()   # Set model to training mode
     train_loss = 0.0
+    total_imgs = 0
 
     for batch_idx, x_0 in enumerate(train_loader):
         optimizer.zero_grad()
         x_0 = x_0.to(DEVICE)
         
         # t ~ Uniform ({1...T})
-        t = torch.randint(low=0, high=NUM_TIMESTEPS, size=(BATCH_SIZE, )).to(DEVICE)
+        t = torch.randint(low=0, high=NUM_TIMESTEPS, size=(x_0.size(0), )).to(DEVICE)
 
         # noise ~ N(0, I)
         noise = torch.randn_like(x_0).to(DEVICE)
@@ -81,12 +104,16 @@ for epoch in range(EPOCHS):
 
         ema_model.update_parameters(model)
 
-        if (batch_idx+1) % 9 == 0:
-            print(f"Batch: {batch_idx+1}/{len(train_loader)} | Loss: {loss.item():.5f}")
+        if (batch_idx) % 6 == 0:
+            print(f"Batch: {batch_idx}/{len(train_loader)} | Loss: {loss.item():.5f}")
 
         train_loss += loss.item() * x_0.size(0)
+        wandb.log({"batch_loss": train_loss, "batch_idx": batch_idx})
+        total_imgs += x_0.size(0)
     
-    epoch_train_loss = train_loss / (len(train_loader)*BATCH_SIZE)
+    # In case the total dataset size is not divisible by batch_size, we explicitly count number of images in last batch and add them for correct division to get epoch_loss
+    epoch_train_loss = train_loss / (total_imgs)
+    wandb.log({"epoch_loss": epoch_train_loss, "epoch": epoch})
 
     # Saving best performing model
     if epoch_train_loss < best_loss:
@@ -95,9 +122,14 @@ for epoch in range(EPOCHS):
 
     losses.append(epoch_train_loss)
 
-    if (epoch+1) % 5 == 0:
+    if (epoch+1) % 2 == 0:
         print(f"Epoch: {epoch+1}/{EPOCHS} | Loss: {epoch_train_loss:.5f}")
         save_model_checkpoint(f"./outputs/model_epoch_{epoch+1}.pt", epoch, model, ema_model, optimizer, losses)
+        # sampling logic
+        ema_model.eval()
+        with torch.inference_mode():
+            generated = lns.sampling(ema_model.module)
+        # log generated samples to w&b
 
 # Create the plot
 # plt.figure(figsize=(16, 10))